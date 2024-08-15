aaf49653434a8eb7034553699d790b2d96eda2c8
yisus82 <djalma8@gmail.com>
2024-08-15 16:38:57 +0200

    Add logout button in tabs

--- 02-Football/app/(tabs)/_layout.tsx
+++ 02-Football/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Tabs } from "expo-router";
-import { Pressable } from "react-native";
+import { router, Tabs } from "expo-router";
+import { Alert, Pressable } from "react-native";
 
 type TabBarIconProps = {
   name: React.ComponentProps<typeof FontAwesome>["name"];
@@ -9,44 +9,81 @@ type TabBarIconProps = {
 
 const TabBarIcon = (props: TabBarIconProps) => <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 
-const TabLayout = () =>
-  <Tabs>
-    <Tabs.Screen
-      name="teams"
-      options={{
-        title: "Teams",
-        tabBarIcon: ({ color }) => <TabBarIcon name="shield" color={color} />,
-        headerRight: () =>
-          <Pressable>
-            {({ pressed }) => (
-              <FontAwesome
-                name="plus-circle"
-                size={25}
-                color="black"
-                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-              />
-            )}
-          </Pressable>,
-      }}
-    />
-    <Tabs.Screen
-      name="players"
-      options={{
-        title: "Players",
-        tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-        headerRight: () =>
-          <Pressable>
-            {({ pressed }) => (
-              <FontAwesome
-                name="plus-circle"
-                size={25}
-                color="black"
-                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-              />
-            )}
-          </Pressable>,
-      }}
-    />
-  </Tabs>;
+const TabLayout = () => {
+  const logout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      { text: "Cancel" },
+      {
+        text: "Logout", onPress: () => {
+          router.dismissAll();
+          router.push("/");
+        }
+      },
+    ]);
+  };
+
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="teams"
+        options={{
+          title: "Teams",
+          tabBarIcon: ({ color }) => <TabBarIcon name="shield" color={color} />,
+          headerLeft: () =>
+            <Pressable onPress={logout}>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="sign-out"
+                  size={25}
+                  color="black"
+                  style={{ marginLeft: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>,
+          headerRight: () =>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="plus-circle"
+                  size={25}
+                  color="black"
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>,
+        }}
+      />
+      <Tabs.Screen
+        name="players"
+        options={{
+          title: "Players",
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          headerLeft: () =>
+            <Pressable onPress={logout}>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="sign-out"
+                  size={25}
+                  color="black"
+                  style={{ marginLeft: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>,
+          headerRight: () =>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="plus-circle"
+                  size={25}
+                  color="black"
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>,
+        }}
+      />
+    </Tabs>
+  );
+};
 
 export default TabLayout;
